8b43f8723722a5e55c743c7badf4419cf2957249
a71sa <a71sa1997@gmail.com>
2021-07-08 19:08:43 +0430

    show to pro

--- dvsr.py
+++ dvsr.py
@@ -1,9 +1,29 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from IPython import get_ipython
+
+# %%
 import time
 import numpy as np
 import cv2
 from numpy import int16, uint8, log2
 import pydvs.generate_spikes as gs
+import cython
+from testingcode.myfilters import singlenoisefilter
 
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# %% [markdown]
+# 
+# 
+# 
+# 
+# 
+# # defines
+
+# %%
 
 # -------------------------------------------------------------------- #
 # grab / rescale frame                                                 #
@@ -27,7 +47,8 @@ def grab_frame(dev, width, height, col_from, col_to):
 
   return img
 
-#-------------------------------------------------
+
+# %%
 #----------------------------------------------------------------------#
 #mode 16 32 64 128
 #0 up polar 1 down 2 merge pollar
@@ -46,7 +67,7 @@ data_mask = uint8(cam_res - 1)
 
 polarity = 2
 
-history_weight = 1.0
+history_weight = 1.0#1.0
 threshold = 12 # ~ 0.05*255
 max_threshold = 180 # 12*15 ~ 0.7*255
 
@@ -71,7 +92,15 @@ spike_lists = None
 pos_spks = None
 neg_spks = None
 max_diff = 0
-#-----------------------------------------------------
+
+
+# %%
+
+
+
+
+
+
 
 # -------------------------------------------------------------------- #
 # inhibition related                                                   #
@@ -99,32 +128,17 @@ fps = video_dev.get(cv2.CAP_PROP_FPS)
 if fps == 0.0:
   fps = 125.0
 max_time_ms = int(1000./float(fps))
-#-------------------------------------------------------------------
-#--------------------------------------------------------------------
-#----------------------------------------------------------------
-#main algorithm
-#---------------------- main loop -------------------------------------#
-LOOP_START =True
-if video_dev.isOpened()==False:
-    print("error in camera")
-    LOOP_START = False
-    
 
-WINDOW_NAME = 'spikes'
-cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_AUTOSIZE)
-cv2.startWindowThread()
-
-is_first_pass = True
-start_time = time.time()
-end_time = 0
-frame_count = 0
-
-pTime =0
-curr[:], scale_width, scale_height, col_from, col_to = grab_first(video_dev, cam_res)
-while(LOOP_START):
-  # get an image from video source
+# %%
+def Base_work(curr,spikes):
+    # get an image from video source
   curr[:] = grab_frame(video_dev, scale_width,  scale_height, col_from, col_to)
-  
+  #curr = cv2.medianBlur(curr,5)
+  imgraw=np.zeros((128,128,3),np.uint8)
+  imgraw[:,:,0]=curr
+  imgraw[:,:,1]=curr
+  imgraw[:,:,2]=curr
+ 
   # do the difference
   diff[:], abs_diff[:], spikes[:] = gs.thresholded_difference(curr, ref, threshold)
   
@@ -155,43 +169,41 @@ while(LOOP_START):
   
   spk_img[:] = gs.render_frame(spikes, curr, cam_res, cam_res, polarity)
 
-  #cTime = time.time()
-  #fps = 1 / (cTime - pTime)
-  #pTime = cTime
-    
   #show
   fm=np.copy(spikes.astype(uint8))
   cv2.imshow (WINDOW_NAME, spk_img.astype(uint8)) 
   cv2.putText(fm,f'FPS: {int(fps)}',(0,0),cv2.FONT_HERSHEY_PLAIN,3,(8,255,8),3)
+  return imgraw
 
-  #---------------------------------------------------
+
+# %%
+
+
+
+# %%
+def get_white_frame(spikes):
   rows,cols = np.where(spikes==-1)
-  imges=np.zeros([128,128,1],np.uint8)
+  imgesR=np.zeros([width,height,1],np.uint8)
+  imgesR[rows,cols]=255
+  rows,cols = np.where(spikes==1)
+  imgesG=np.zeros([width,height,1],np.uint8)
+  imgesG[rows,cols]=255
+  rows,cols = np.where(spikes!=0)
+  imges=np.zeros([width,height,1],np.uint8)
   imges[rows,cols]=255
+  return imgesR,imgesG,imges
 
-  kernel = np.ones((3,3),np.uint8)
-  #opening = cv2.morphologyEx(fm, cv2.MORPH_OPEN, kernel)
-  closing = cv2.morphologyEx(imges, cv2.MORPH_CLOSE, kernel)
-  dilation = cv2.dilate(closing,kernel,iterations = 3)
-  opening = cv2.morphologyEx(dilation, cv2.MORPH_OPEN, kernel)
 
-  img=opening
+# %%
+def drawobject(figname,img):
 
-  cv2.imshow ("spikes1", img) 
-
-  # Take only region of logo from logo image.
-  img2_fg = cv2.bitwise_and(spk_img,spk_img,mask = img) 
-  cv2.imshow('output',img2_fg)
-  #contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-  #cv2.drawContours(img,contours,-1,(255,255,255),3)  
-  #cv2.imshow('output',img)
 
   ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
   contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
   #img=cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-  imges11=np.zeros([128,128,3],np.uint8)
-  cv2.drawContours(imges11,contours,-1,(255,0,255),3)
-  cv2.imshow('output1',imges11)
+  imges11=np.zeros([width,height,3],np.uint8)
+  cv2.drawContours(imges11,contours,-1,(80,60,0),3)
+  #cv2.imshow('output1',imges11)
 
   try: hierarchy = hierarchy[0]
   except: hierarchy = []
@@ -199,17 +211,111 @@ while(LOOP_START):
   min_x, min_y = width, height
   max_x = max_y = 0
 
-  # computes the bounding box for the contour, and draws it on the frame,
+  ar=[]
+      # computes the bounding box for the contour, and draws it on the frame,
   for contour, hier in zip(contours, hierarchy):
       (x,y,w,h) = cv2.boundingRect(contour)
       min_x, max_x = min(x, min_x), max(x+w, max_x)
       min_y, max_y = min(y, min_y), max(y+h, max_y)
-      if w > 10 and h > 10  and w<150 and h<150:
-          cv2.rectangle(imges11, (x,y), (x+w,y+h), (255, 0, 0), 2)
+      if w > 40 and h > 40  and w<150 and h<150:
+          cv2.rectangle(imges11, (x,y), (x+w,y+h), (0, 0, 255), 2)
+          ar.append([x,y,x+w,x+h])
+  #if max_x - min_x > 10 and max_y - min_y > 10 and max_y - min_y < 150 and max_x - min_x<150:
+  #    cv2.rectangle(imges11, (min_x, min_y), (max_x, max_y), (255, 0, 0), 2)
+  cv2.imshow(figname,imges11)
+  return ar
 
-  if max_x - min_x > 10 and max_y - min_y > 10 and max_y - min_y < 150 and max_x - min_x<150:
-      cv2.rectangle(imges11, (min_x, min_y), (max_x, max_y), (255, 0, 0), 2)
-  cv2.imshow('output2',imges11)
+
+# %%
+def base_filter(current,imgesR,imgesG,imges):
+  rir=imgesR
+  rig=imgesG
+  rii=imges
+
+  #sow all
+  Hori = np.concatenate((imges, imgesR), axis=1)
+  Hori = np.concatenate((Hori, imgesG), axis=1)
+  cv2.imshow ("Hori   N  R   G", Hori)
+  #sow filtered
+  fi=np.zeros_like(rii)
+  fr=np.zeros_like(rir)
+  fg=np.zeros_like(rig)
+  singlenoisefilter(fr,rir,128,3)
+  singlenoisefilter(fg,rig,128,3)
+  singlenoisefilter(fi,rii,128,3)
+  rir.
+
+  Hori2 = np.concatenate((rii, rir), axis=1)
+  Hori2 = np.concatenate((Hori2, rig), axis=1)
+  cv2.imshow ("filtered   N  R   G", Hori2)
+
+  #--------------------------------
+  #adding some thing
+  kernel = np.ones((5,5),np.uint8)
+  dili = cv2.dilate(rii,kernel,iterations=1)
+  dilr = cv2.dilate(rir,kernel,iterations=5)
+  dilg = cv2.dilate(rig,kernel,iterations=1)
+
+  Hori3 = np.concatenate((dili, dilr), axis=1)
+  Hori3 = np.concatenate((Hori3, dilg), axis=1)
+  cv2.imshow ("dil   N  R   G", Hori3)
+
+  #draw count
+  ar=drawobject("r countur",dilr)
+
+  cu=current
+  for x in ar:
+      cv2.rectangle(cu, (x[0],x[1]), (x[2],x[3]), (0, 0, 255), 2)
+      cv2.putText(cu,"O",(x[0],x[1]),cv2.FONT_HERSHEY_PLAIN,2,(8,255,8),1)
+  
+  cv2.imshow ("object", cu)
+
+
+
+
+  imgG = dilg
+  imgR = dilr
+  img  = dili
+
+  return img,imgG,imgR
+
+
+# %%
+
+
+
+# %%
+#---------------------- main loop -------------------------------------#
+LOOP_START =True
+if video_dev.isOpened()==False:
+    print("error in camera")
+    LOOP_START = False
+    
+
+WINDOW_NAME = 'spikes'
+cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_AUTOSIZE)
+cv2.startWindowThread()
+
+is_first_pass = True
+start_time = time.time()
+end_time = 0
+frame_count = 0
+
+pTime =0
+curr[:], scale_width, scale_height, col_from, col_to = grab_first(video_dev, cam_res)
+while(LOOP_START):
+  #get spikes and prepare them  
+  d3img=Base_work(curr,spikes)
+
+  #get spike on white frame
+  imgesR,imgesG,imges = get_white_frame(spikes)
+
+  #---------------------------------------------------
+  #first filter
+  img,imgG,imgR = base_filter(d3img,imgesR.squeeze(),imgesG.squeeze(),imges.squeeze())
+
+ 
+  #==============================================================
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
   
@@ -223,3 +329,11 @@ while(LOOP_START):
     frame_count += 1
 
 cv2.destroyAllWindows()
+
+
+# %%
+
+# %%
+
+
+
